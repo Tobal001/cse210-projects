9a3b85e5f83ec19f8a882956c4de8874f2daacbe
Cristobal Henriquez <brohenriquez@gmail.com>
2024-09-17 15:49:04 -0400

    C# Prep 1 compleated

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string FirstName = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {FirstName} {LastName}.");
+
+
+
+
     }
 }
