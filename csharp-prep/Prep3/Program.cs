47dcc6fddd02c715949923a6be4c21856ec33325
Cristobal Henriquez <brohenriquez@gmail.com>
2024-09-20 15:19:49 -0400

    Prep3 Compleate

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        
+        int randomNumber = randomGenerator.Next(1,10); 
+        Console.WriteLine($"What is the magic number? {randomNumber}");
+       
+        int guess = -1;
+        int guessCount = 1;
+
+        while (guess != randomNumber)
+        {   
+            guessCount++;
+            Console.Write("What is your number?");  
+            guess = int.Parse(Console.ReadLine());
+
+            if (randomNumber > guess)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (randomNumber < guess)
+                {
+                    Console.WriteLine("Guess lower");
+                }
+
+            else 
+            {
+                Console.WriteLine($"You guessed it, you guessed {guessCount} times");
+            }
+
+        }
+
     }
 }
