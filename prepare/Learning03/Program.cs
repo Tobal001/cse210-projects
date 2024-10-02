dc8648204fbe10cb15e9c853e3830c9de8ea1d53
Cristobal Henriquez <brohenriquez@gmail.com>
2024-10-02 11:41:19 -0400

    Compleated prepare/learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction ex = new Fraction();
+        ex.SetTop(1);
+        Console.WriteLine(ex.GetFractionString());
+        Console.WriteLine(ex.GetDecimalValue());
+
+        ex.SetTop(5);
+        Console.WriteLine(ex.GetFractionString());
+        Console.WriteLine(ex.GetDecimalValue());
+
+        ex.SetTop(3);
+        ex.SetBottom(4);
+        Console.WriteLine(ex.GetFractionString());
+        Console.WriteLine(ex.GetDecimalValue());
+
+        ex.SetTop(1);
+        ex.SetBottom(3);
+        Console.WriteLine(ex.GetFractionString());
+        Console.WriteLine(ex.GetDecimalValue());
+
+        
+
     }
 }
